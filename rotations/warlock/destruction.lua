391b0a1063ed6b756bd199f8e73783684e4fa5c8
Kinkz <blamejimmy@gmail.com>
2013-11-03 00:41:24 -0400

    Initial Commit
    From Kink <3.

---
+++ rotations/warlock/destruction.lua
@@ -0,0 +1,15 @@
+--[[
+        ==============================================================
+        Mentally's Hysteria - A ProbablyEngine Custom Rotation Package
+        - Created and being developed by, Kink.
+        
+        :: Hysteria Destruction Warlock PvE Profile
+        
+        Help, suggestions and discussion thread:
+        https://probablyengine.com/forum/viewtopic.php?id=107
+        ==============================================================
+        Updated: 12:40 AM 03.11.2013
+]]
+ProbablyEngine.rotation.register_custom(258, "HysteriaDestruction", {
+        -- Rotation
+})
