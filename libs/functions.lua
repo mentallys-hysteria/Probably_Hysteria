fbc71c85fb310e5184d9ec3168a6c08ec76f89dc
Elizabeth Victoria Truehearth <e.truehearth@gmail.com>
2013-11-03 02:31:50 +0100

    Initial Commit
    Initial commit of Hysteria's ProbablyEngine rotations.
    More to come very soon.
    
    - Added Shadow Priest engine

---
+++ libs/functions.lua
@@ -0,0 +1,13 @@
+--[[
+	==============================================================
+	Mentally's Hysteria - A ProbablyEngine Custom Rotation Package
+	- Based upon Mentally's Hysteria PQR Rotations
+	
+	:: Function Helper file for Hysteria Profiles
+	
+	Help, suggestions and discussion thread:
+	https://probablyengine.com/forum/viewtopic.php?id=107
+	==============================================================
+	Updated: 20:12 01.11.2013
+]]
+hysteria = {}
---
+++ rotations/priest/rotation.lua
@@ -0,0 +1,17 @@
+--[[
+	==============================================================
+	Mentally's Hysteria - A ProbablyEngine Custom Rotation Package
+	- Based upon Mentally's Hysteria PQR Rotations
+	
+	:: Hysteria Shadow Priest PvE Profile
+	
+	Help, suggestions and discussion thread:
+	https://probablyengine.com/forum/viewtopic.php?id=107
+	==============================================================
+	Updated: 20:12 01.11.2013
+]]
+ProbablyEngine.rotation.register_custom(258, "HysteriaPriest", {
+	-- Rotation
+}, {
+	-- Always keep these buffs active
+})
